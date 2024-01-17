91123c8f9dfb07262618534b42569692948fc5f0
James Thompson <100stacks@users.noreply.github.com>
2024-01-17 15:43:39 -0600

    actions: `Branch Protection` demo

--- ci.py
+++ ci.py
@@ -1,3 +1,6 @@
 import wandb
 
 print(f'The version of WandB is: {wandb.__version__}')
+
+# assert TEST to show BRANCH PROTECTION based on GitHub Action job (NOT WORKFLOW NAME!)
+assert wandb.__version__ == '2.0.24', f'Expected version 2.0.24, instead got {wandb.__version__}'
